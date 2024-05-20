6ebcaa2fb8b5a1aa00db2a4b8dc30a7eb921336e
MatteoCantafio25 <mattyteo004@gmail.com>
2024-05-20 14:47:10 +0200

    Added exercise guideline as a comment

--- src/org/learning/oop/shop/Product.java
+++ src/org/learning/oop/shop/Product.java
@@ -12,7 +12,8 @@ public class Product {
         - descrizione
         - prezzo
         - iva
-        Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+        Usate opportunamente i livelli di accesso (public, private), i costruttori,
+         i metodi getter e setter ed eventuali altri metodi di “utilità”per fare in modo che:
         - alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
         - il codice prodotto sia accessibile solo in lettura e deve essere restituito come Stringa con un padleft a 6 caratteri (es. codice 15->“000015")
         - gli altri attributi siano accessibili sia in lettura che in scrittura
@@ -20,6 +21,17 @@ public class Product {
         - il prodotto esponga un metodo per avere il prezzo comprensivo di iva
         - il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
         Nello stesso package aggiungete una classe Main con metodo main nella quale testate tutte le funzionalità della classe Prodotto.
+
+
+        Lo shop gestisce diversi tipi di prodotto:
+        Smarphone, caratterizzati anche dal codice IMEI e dalla quantità di memoria
+        Televisori, caratterizzati dalle dimensioni e dalla proprietà di essere smart oppure no
+        Cuffie, caratterizzate dal colore e se sono wireless o cablate
+        Utilizzate l’ereditarietà per riutilizzare il codice di Prodotto nella stesura delle classi che gestiscono i vari sotto tipi di prodotto.
+        Create una classe Carrello con metodo main, in cui chiedete all’utente di valorizzare un carrello di prodotti con dati inseriti tramite scanner.
+        Durante la richiesta di valorizzazione chiedete all’utente se sta inserendo uno Smarphone o un Televisore o Cuffie
+        e in base alla scelta dell’utente utilizzate il costruttore opportuno.
+        Al termine dell’inserimento stampate il carrello (fate l’override del metodo toString per restituire le informazioni da stampare per ogni classe)
         */
 
 
